da440afb05a1b54a0a803e78c358a98631569814
masaki.okabe <okabe.kklab@gmail.com>
2020-06-25 16:43:57 +0900

    add display burger feature.

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger.js"
+
+class BurgerBuilder extends Component{
+
+    state = {
+        ingredients: {
+            salad: 1,
+            cheese: 2,
+            bacon: 1,
+            meat: 2
+        }
+    }
+
+    render(){
+        return (
+          <Aux>
+              <Burger ingredients={this.state.ingredients}/>
+              <div>Build Controls</div>
+          </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
